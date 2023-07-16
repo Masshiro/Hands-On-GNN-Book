5e48fdf316c3c87a01262271ff543d243a64a649
masshiro <kwanwaimasshiro@gmail.com>
2023-07-16 12:59:52 +0800

    update

--- Further/DiveToDeepL/code/d2l_utils.py
+++ Further/DiveToDeepL/code/d2l_utils.py
@@ -85,8 +85,6 @@ def load_data_fashion_mnist(batch_size, resize=None):
             data.DataLoader(mnist_test, batch_size, shuffle=False,
                             num_workers=get_dataloader_workers()))
 
-
-
 class Accumulator:
     """在n个变量上累加"""
     def __init__(self, n):
@@ -101,6 +99,80 @@ class Accumulator:
     def __getitem__(self, idx):
         return self.data[idx]
 
+def accuracy(y_hat, y):
+    """定义分类精度"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype)==y
+    return float(cmp.type(y.dtype).sum())
+
+def evaluate_accuracy(net, data_iter):
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval() # 将模型设置为评估模式
+    metric = Accumulator(2) # 正确预测数、预测总数
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+def sgd(params, lr, batch_size):
+    """小批量随机梯度下降"""
+    with torch.no_grad():           # 上下文管理器来禁用自动求导功能，因为我们不需要计算参数的二阶导数
+        for param in params:
+            # 参数更新方法：
+            # 用参数当前的值减去学习率乘以参数当前的梯度除以小批量数据的大小
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """训练一个迭代周期"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater, draw=False):
+    """训练模型"""
+    all_train_loss = []
+    all_train_acc = []
+    all_test_acc = []
+
+    for epoch in range(num_epochs):
+        print("epoch ", epoch)
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        all_train_loss.append(train_metrics[0])
+        all_train_acc.append(train_metrics[1])
+        all_test_acc.append(test_acc)
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+    if draw:
+        y = [list(all_train_acc), list(all_train_loss), list(all_test_acc)]
+        x = [i+1 for i in range(num_epochs)]
+        # print("x", x)
+        # print("y", y)
+        linenames = ["train acc", "train loss", "test acc"]
+        plot_train_result(x, y, linenames=linenames, xlabel="epoch", ylim=(0.2, 1.0), xlim=(0.5, 10.5), legend=True)
 
 
 
--- Further/DiveToDeepL/note/4-MLP.md
+++ Further/DiveToDeepL/note/4-MLP.md
@@ -17,3 +17,69 @@
 
 **从线性到非线性**
 
+- 符号：
+  - $\mathbf{X}\in \mathbb{R}^{n\times d}$: $n$个样本的小批量，每个样本$d$个输入特征
+  - $\mathbf{H}\in\mathbb{R}^{n\times h}$：隐藏层输出，称为隐藏表示(hidden representations)
+    - $\mathbf{W}^{(1)}\in\mathbb{R}^{d\times h}$：隐藏层权重
+    - $\mathbf{b}^{(1)}\in\mathbb{R}^{1\times h}$：隐藏层偏置
+  - $\mathbf{O}\in\mathbb{R}^{n\times q}$：输出层输出
+    - $\mathbf{W}^{(2)}\in\mathbb{R}^{h\times q}$：输出层权重
+    - $\mathbf{b}^{(2)}\in\mathbb{R}^{1\times q}$：输出层偏置
+
+- 合并隐藏层，得到等价的单层模型：
+  $$
+  \begin{aligned}
+  \mathbf{O} &=(\mathbf{XW}^{(1)}+\mathbf{b}^{(1)} )\mathbf{W}^{(2)}+\mathbf{b}^{(2)}\\
+  &=\mathbf{XW}+\mathbf{b}
+  \end{aligned}
+  $$
+
+  - $\mathbf{W}=\mathbf{W}^{(1)}\mathbf{W}^{(2)}$, $\mathbf{b}=\mathbf{b}^{(1)}\mathbf{W}^{(2)}+\mathbf{b}^{(2)}$
+
+- 对每个隐藏单元应用非线性的激活函数以发挥多层架构的潜力：
+
+  - 有了激活函数，多层感知机不会退化为线性模型：
+    $$
+    \begin{aligned}
+    \mathbf{H} &= \sigma(\mathbf{XW}^{(1)}+\mathbf{b}^{(1)})\\
+    \mathbf{O} &= \mathbf{HW}^{(2)}+\mathbf{b}^{(2)}
+    \end{aligned}
+    $$
+
+## 2 激活函数
+
+- 通过计算加权和并加上偏置来确定神经元是否应该被激活
+
+**ReLU**
+
+- 实现简单，对给定元素$x$，ReLU函数被定义为该元素与$0$的最大元素
+  $$
+  \text{ReLU}=\max(x,0)
+  $$
+
+- 有很多变体：
+
+  - 参数化ReLU
+    $$
+    p\text{ReLU}(x)=\max(x,0)+\alpha \min(0,x)
+    $$
+
+**sigmoid**
+
+- 通常称为压缩函数：将范围$(-\infty,\infty)$中任意输出压缩至$(0,1)$
+
+$$
+\text{sigmoid}(x)=\frac{1}{1+\exp(-x)}
+$$
+
+- sigmoid在隐藏层中已经较少使⽤，它在⼤部分时候被更简单、 更容易训练的ReLU所取代
+
+**tanh**
+
+- tanh(双曲正切)函数也能将其输⼊压缩转换到区间$(-1,1)$上
+  $$
+  \tanh(x)=\frac{1-\exp(-2x)}{1+\exp(-2x)}
+  $$
+
+## 3 模型选择、欠/过拟合
+
